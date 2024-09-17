1e743c229f0e6d9486fb33abba87996253a5c970
vzk onty <vezorakey@gmail.com>
2024-09-17 19:57:48 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
 global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.MONGO_DB_URI = "mongodb+srv://vezorakey:Zorachan123@cluster0.ahjhu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "vezorakey@gmail.com" //email
+global.email_password = "hgvmbbvcqoxvhviq" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
